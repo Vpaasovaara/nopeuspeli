3e987806aa8b3c27fe60f119e47927d8690ed2af
Ville <ville@pop-os.localdomain>
2021-09-27 19:10:55 +0300

    a

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ mongoose.connect(config.MONGO_URI, {
 })
 
 app.use(cors())
-// app.use(express.static('build'))
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('build'))
+/*app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+});*/
 
 
 //app.use(middleware.homepage)
